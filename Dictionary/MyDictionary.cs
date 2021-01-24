c4bf138253b0032be278693a80bda6b0b9896997
Selmanerdemm <selmanerdem3447@gmail.com>
2021-01-24 21:53:07 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] item;
+        TValue[] items;
+
+        public MyDictionary()
+        {
+            item = new TKey[0];
+            items = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] keys = item;
+            TValue[] values = items;
+            item = new TKey[item.Length + 1];
+            items = new TValue[items.Length + 1];
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                item[i] = keys[i];
+
+            }
+            for (int i = 0; i < values.Length; i++)
+            {
+                items[i] = values[i];
+            }
+
+            item[item.Length - 1] = key;
+            items[items.Length - 1] = value;
+
+
+        }
+
+        public void Display()
+        {
+
+            for(int i = 0; i < item.Length; i++)
+            {
+                Console.WriteLine($" {item[i]} {items[i]}");
+            }
+        }
+
+        
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Selman Erdem");
+            myDictionary.Add(2,"Cemal Kaya");
+            myDictionary.Add(3,"Furkan Pala");
+            myDictionary.Add(4, "Mustafa Dümen");
+            myDictionary.Add(5,"Filiz Akın");
+            
+
+            myDictionary.Display();
+
+
+
+            //var kullanicilar = new Dictionary<int, string>();
+
+            //kullanicilar.Add(1,"Selman Erdem");
+            //kullanicilar.Add(2, "Cemal Kaya");
+            //kullanicilar.Add(3,"Furkan Pala");
+            //kullanicilar.Add(4,"Mustafa Dümen");
+
+            //foreach (KeyValuePair<int,string> kullanici in kullanicilar)
+
+            //{
+            //    Console.WriteLine("ID: "+kullanici.Key + " " + "Full Name: "+ kullanici.Value);
+                
+            //}
+
+            //string yeniKullanici = "Filiz Şahap";
+            //bool varmi = kullanicilar.TryGetValue(0, out yeniKullanici);
+
+            //if (varmi)
+            //{
+            //    Console.WriteLine("Bu Id de bir kullanıcı mevcuttur.");
+            //}
+            //else
+            //{
+            //    kullanicilar.Add(0,yeniKullanici);
+            //    Console.WriteLine("Ekleme işlemi başarıyla gerçekleşti"+);
+                
+            //}
+        }
+    }
+}
